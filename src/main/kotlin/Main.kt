75bbd0d0af94561157a5a985cfbba3541d53abd5
StefanGherghel <stefan.gherghel@student.tuiasi.ro>
2023-05-11 18:11:01 +0300

    Corutine asincrone si lista

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,36 @@
 import kotlinx.coroutines.*
 import kotlin.system.*
 import kotlinx.coroutines.channels.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 var ADT = mutableListOf<Int>()
+var rezz = mutableListOf<Int>()
+val mutex = Mutex(false)
 
-suspend fun stage1Pipeline(lista: MutableList<Int>) {
-    val alfa = 2
-    for(i in 0..10) {
-        lista[i] = lista[i] * alfa
-    }
-}
-
-suspend fun stage2Pipeline(lista: MutableList<Int>) {
-    lista.sort()
-}
-
-suspend fun stage3Pipeline(lista: MutableList<Int>) {
-    lista.forEach { print(it.toString() +", ")}
+suspend fun Suma(n: Int):Int
+{
+    return n*(n-1)/2
 }
 
 fun main() = runBlocking {
+    for(i in 1..4)
+        ADT.add(i)
 
-    for(i in 0..10)
-    {
-        ADT.add(10 - i)
-    }
+    var jobs = mutableListOf<Job>()
+
     var time = measureTimeMillis {
-        val PIPE1 = async {
-            stage1Pipeline(ADT)
-            delay(100)
+        for (i in 1..4) {
+            jobs.add(
+                async{
+                    mutex.withLock {
+                        rezz.add(Suma(ADT[0]))
+                        ADT.removeAt(0)
+                        }
+                    })
         }
-
-        val PIPE2 = async {
-            PIPE1.await()
-            stage2Pipeline(ADT)
-            delay(1000)
-        }
-
-        val PIPE3 = async {
-            PIPE2.await()
-            stage3Pipeline(ADT)
-            delay(500)
-        }
-        PIPE3.await()
     }
-    println("Time: +$time")
+    jobs.forEach(){it.join()}
+    rezz.forEach(){println("valoare rezultat: "+it.toString())}
+    println("Timp scurs: $time")
 }
